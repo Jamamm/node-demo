08f7ee1ca090ef5b20717dbf567f445d35f893cb
jamamm <iamdengjian@gmail.com>
2018-01-30 22:42:46 +0800

    更新server.js

--- server.js
+++ server.js
@@ -10,50 +10,32 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
-  console.log('HTTP 路径为\n' + path)
-  if(path == '/style.css'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: red;}')
-    response.end()
-  }else if(path == '/main.js'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("这是JS执行的")')
-    response.end()
-  }else if(path == '/'){
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.css">' +
-      '</head><body>'  +
-      '<h1>你好</h1>' +
-      '<script src="/main.js"></script>' +
-      '</body></html>')
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('哈哈哈')
     response.end()
   }else{
     response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
     response.end()
   }
-  
-  
-  /******** 
-   * 
-   * 用户请求 / 时，返回 html 内容
-   * 该 html 内容里面由一个 css link 和一个 script
-   * css link 会请求 /style.css，返回 css 内容
-   * script 会请求 /main.js，返回 js 内容
-   * 请求 / /style.css /main.js 以外的路径，则一律返回 404 状态码代码结束，
-   * 
-   * 
-   * 下面不要看 ************/
+
+  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
