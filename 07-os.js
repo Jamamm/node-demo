b934082e25e777870e346d243b69a42aea35f0bd
dengjian <dengjian@jsmscp.com>
2019-01-15 14:32:44 +0800

    看看操作系统

--- 07-os.js
+++ 07-os.js
@@ -1,2 +1,7 @@
 var os = require('os')
 console.log(os.cpus())
+console.log(os.networkInterfaces())
+console.log(os.networkInterfaces())
+console.log(os.networkInterfaces())
+console.log(os.networkInterfaces())
+console.log(os.networkInterfaces())
