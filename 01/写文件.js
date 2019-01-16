da505301669013505e53132fe7bec55e628d50fc
dengjian <dengjian@jsmscp.com>
2019-01-16 13:33:49 +0800

    调整了一下目录结构，可能会有目录错误没改过来

--- 01/写文件.js
+++ 01/写文件.js
@@ -1,6 +1,6 @@
 var writeTxt = require('fs')
 
-writeTxt.writeFile('./data/hello.md','hhh偶在写文件呀', function (error) {
+writeTxt.writeFile('.././data/hello.md','hhh偶在写文件呀', function (error) {
     if(error){
        console.log("写文件失败！")
     }else{
--- 01/读取文件.js
+++ 01/读取文件.js
@@ -1,6 +1,6 @@
 var qwe = require('fs');
 
-qwe.readFile('./data/hello.txt', function (error,data) {
+qwe.readFile('.././data/hello.txt', function (error,data) {
     if(error){
       console.log("文件读取失败")
     }else{
