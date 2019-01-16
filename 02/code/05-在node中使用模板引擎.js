2a0af9dbffcead223ef05869d37cd41801ebc019
dengjian <dengjian@jsmscp.com>
2019-01-16 14:17:05 +0800

    在node中使用模板引擎

---
+++ 02/code/05-在node中使用模板引擎.js
@@ -0,0 +1,40 @@
+var template = require('art-template')
+
+var fs = require('fs')
+
+// var tplStr = `
+//         <!DOCTYPE html>
+//         <html lang="en">
+//         <head>
+//             <meta charset="UTF-8">
+//             <title>Title</title>
+//         </head>
+//         <body>
+//             <p> 大家好，我叫： {{ name }} </p>
+//             <p> 我今年 {{ age }} 岁了 </p>
+//             <p> 我来自 {{ province }} </p>
+//             <p> 我喜欢：{{ each hobbies }} {{ $value }} {{ /each }} </p>
+//         </body>
+//         </html>
+// `
+
+fs.readFile('./tpl.html', function (err, data) {
+    if(err){
+        return console.log("读取文件失败")
+    }
+
+    var ret = template.render(data.toString(), {
+        name: 'Jam',
+        age: 16,
+        province: '北京',
+        hobbies: [
+            '吃饭',
+            '睡觉',
+            '打豆豆'
+        ]
+    })
+    console.log(ret)
+})
+
+
+
