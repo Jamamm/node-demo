967bea581f41886caea8f0d93180facf2c64a342
dengjian <dengjian@jsmscp.com>
2019-01-16 09:39:42 +0800

    node中没有全局作用域，只有模块作用域，外部访问不到内部，内部也访问不到外部

--- 09-简单的模块化/a.js
+++ 09-简单的模块化/a.js
@@ -1,3 +1,11 @@
 console.log("a start")
+
+var foo = 'aaa'
+var fbb = 'aaaaaaa'
+
 require('./b.js')
+
 console.log('a end')
+// node中没有全局作用域，只有模块作用域
+
+console.log('foo 的值是' + foo)
--- 09-简单的模块化/b.js
+++ 09-简单的模块化/b.js
@@ -1,1 +1,9 @@
-console.log("b.js被加载了")
+console.log("b.js start")
+
+var foo = 'bbbb'
+
+console.log(fbb)  //会报错，作用域不同
+
+require('./c.js')
+
+console.log("b.js end")
