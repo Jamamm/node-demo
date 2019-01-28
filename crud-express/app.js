a5f1459889916cacf6ad422c222c2ffb816d0d38
dengjian <dengjian@jsmscp.com>
2019-01-28 18:32:40 +0800

    学生管理，添加数据

--- crud-express/app.js
+++ crud-express/app.js
@@ -2,14 +2,24 @@ var express = require('express')
 
 var app = express()
 
-app.use('/node_modules', express.static('./node'))
+app.use('/public', express.static('./public'))
+app.use('/node_modules', express.static('./node_modules'))
 
 app.engine('html', require('express-art-template'))
 
 
 
 app.get('/', function (req, res) {
-    res.render("index.html")
+    res.render("index.html",{
+        students: [
+            {"id": 1, "name": "张三", "gender": 1, "age": 12, "hobbies": "看书、睡觉、lol"},
+            {"id": 2, "name": "李四", "gender": 1, "age": 27, "hobbies": "看书、睡觉、lol"},
+            {"id": 3, "name": "王二", "gender": 1, "age": 19, "hobbies": "看书、睡觉、lol"},
+            {"id": 4, "name": "莉莉", "gender": 0, "age": 24, "hobbies": "看书、睡觉、lol"},
+            {"id": 5, "name": "洋洋", "gender": 0, "age": 18, "hobbies": "看书、睡觉、lol"},
+            {"id": 6, "name": "花花", "gender": 0, "age": 16, "hobbies": "看书、睡觉、lol"}
+        ]
+    })
 })
 
 app.listen(5000, function () {
